0a884da52e6c67a3c0b31ae1397e73d117940d0b
LalaIAm <lthorud12@gmail.com>
2023-03-15 14:10:08 -0500

    update config

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env("ADMIN_JWT_SECRET", "LADN269qLdT6ZsaExrZ9Uw=="),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env("API_TOKEN_SALT"),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env("TRANSFER_TOKEN_SALT"),
     },
   },
 });
